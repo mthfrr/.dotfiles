a39ff3161c41e30e38d2de7e42499dc545887764
Mathieu Fourré <mathieu.fourre@epita.fr>
2022-09-06 15:05:24 +0200

    full nerd font p10k, i3-gaps specifig settings in i3, kitty uses Fira Code Nerd

--- install/install-i3.sh
+++ install/install-i3.sh
@@ -3,4 +3,5 @@
 mkdir -p "$I3_CONFIG"
 
 # ln -sf $DOTFILES/i3/lock.png $I3_CONFIG
-ln -sf $DOTFILES/i3/config $I3_CONFIG
+cp $DOTFILES/i3/config $I3_CONFIG
+dot_is_installed i3-gaps && cat $DOTFILES/i3/config-gaps >> $I3_CONFIG
