66a4c76d28b4c1865f0c66b44db79be5caaa457c
Mathieu Fourré <mathieu.fourre@epita.fr>
2022-09-13 09:20:28 +0200

    nvim: change telescope sources

--- nvim/init.lua
+++ nvim/init.lua
@@ -302,9 +302,8 @@ require('gitsigns').setup({
 vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
 vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 vim.keymap.set('n', '<leader>s', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>')
 
 require('telescope').load_extension('fzf')
