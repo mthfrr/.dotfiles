662e00eb9ac4c82de194d217f007f6a9bac4b605
Mathieu Fourré <mathieu.fourre@cybertestsystems.com>
2022-11-02 15:37:35 +0100

    i3: add mediactl to play/pause players

--- install/install-i3.sh
+++ install/install-i3.sh
@@ -2,6 +2,12 @@
 
 mkdir -p "$I3_CONFIG"
 
+if [ ! -f /usr/bin/medialctl ]; then
+  wget -O /tmp/mediactl https://github.com/aaaaaaaalex/mediactl/releases/download/1.0.0/mediactl
+  chmod +x /tmp/mediactl
+  sudo mv /tmp/mediactl /usr/bin/mediactl
+fi
+
 # ln -sf $DOTFILES/i3/lock.png $I3_CONFIG
 cp $DOTFILES/i3/config $I3_CONFIG
 dot_is_installed i3-gaps && cat $DOTFILES/i3/config-gaps >> $I3_CONFIG
