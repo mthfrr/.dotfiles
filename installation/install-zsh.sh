95e790f6651647f9016ce32bb28f8b0f60eea2a9
Mathieu Fourre <mathieu.fourre@epita.fr>
2023-05-30 21:35:23 +0200

    fix(zsh): duplicate zshenv to ZDOTDIR too

--- installation/install-zsh.sh
+++ installation/install-zsh.sh
@@ -4,6 +4,7 @@ mkdir -p "$ZDOTDIR"
 
 ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
 
+ln -sf "$DOTFILES/zsh/zshenv" "$ZDOTDIR/.zshenv"
 ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 ln -sf "$DOTFILES/zsh/zlogout" "$ZDOTDIR/.zlogout"
 ln -sf "$DOTFILES/zsh/p10k.zsh" "$ZDOTDIR/.p10k.zsh"
