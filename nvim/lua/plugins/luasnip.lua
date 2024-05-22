2943e68099533710aa49071116a67cd6db1f532d
Mathieu Fourre <mathieu.fourre@epita.fr>
2024-05-23 00:39:30 +0200

    fix(luasnip): restore functionnality after LazyVim breakage

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,24 @@
 return {
+
   "L3MON4D3/LuaSnip",
   event = "VeryLazy",
   config = function()
     require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/lua_snippets" } })
   end,
+  dependencies = {
+    {
+      "nvim-cmp",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function(_, opts)
+        opts.snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        }
+        table.insert(opts.sources, { name = "luasnip" })
+      end,
+    },
+  },
 }
