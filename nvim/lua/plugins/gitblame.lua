21e9016bf7e2ddb74175b8cdc07ea772ce29142f
pol <pol@local>
2024-06-13 17:57:28 +0200

    feat(nvim): add gitblame plugin

---
+++ nvim/lua/plugins/gitblame.lua
@@ -0,0 +1,15 @@
+return {
+  "f-person/git-blame.nvim",
+  lazy = true,
+  opt = {
+    enabled = false,
+  },
+  cmd = {
+    "GitBlameToggle",
+    "GitBlameOpenCommitURL",
+    "GitBlameCopySHA",
+    "GitBlameCopyCommitURL",
+    "GitBlameOpenFileURL",
+    "GitBlameCopyFileURL",
+  },
+}
