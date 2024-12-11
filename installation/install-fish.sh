bc36dd032d16bd4f4f966c93a8e61312fd6204a3
pol <pol@local>
2024-12-11 11:33:04 +0100

    fix: fish tide configuration

--- installation/install-fish.sh
+++ installation/install-fish.sh
@@ -5,9 +5,11 @@ set -e
 mkdir -p "$XDG_CONFIG_HOME/fish"
 
 for p in config.fish functions completions conf.d; do
-  ln -sf "$DOTFILES/fish/$p" "$XDG_CONFIG_HOME/fish/$p"
+  ln -sf "$DOTFILES/fish/$p" "$XDG_CONFIG_HOME/fish/"
 done
 
 for p in completions conf.d functions; do
   cp -R "$DOTFILES/tide/$p" "$DOTFILES/fish/"
 done
+
+fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Compact --icons='Few icons' --transient=No"
